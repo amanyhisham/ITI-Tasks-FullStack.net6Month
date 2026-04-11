812ee4aced922dce63ec5b0849871cca144302c8
Amany Hisham <amanyhisham43@gmail.com>
2026-04-11 23:51:19 +0200

    designpattern

---
+++ DesignPattern/Lap-DesignPattern/Lap-DesignPattern/GoodCode1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lap_DesignPattern
+{
+    /*
+    Solution
+      Used a private constructor to prevent external instantiation
+      Ensured a single instance using static
+     */
+    class Logger2
+    {
+         private static Logger2 instance2;
+
+         private Logger2() { }
+
+         public static Logger2 GetInstance()
+        {
+            if (instance2 == null)
+            {
+                instance2 = new Logger2();
+            }
+
+            return instance2;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ DesignPattern/Lap-DesignPattern/Lap-DesignPattern/GoodCode2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lap_DesignPattern
+{
+    interface IDiscount
+    {
+        double GetDiscount();
+    }
+
+    class StudentDiscount : IDiscount
+    {
+        public double GetDiscount() => 0.1;
+    }
+
+    class VipDiscount : IDiscount
+    {
+        public double GetDiscount() => 0.2;
+    }
+    //solution
+    //Used an interface
+//Can extend behavior without modifying existing code
+}
